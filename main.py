d4853f7b16b1510f47eca7766ec32dc4669a297e
moreshud <mo.yekini@gmail.com>
2025-06-20 23:16:22 +0200

    chores: add main and inference

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+from awq import AutoAWQForCausalLM
+from transformers import AutoTokenizer
+
+
+token = os.environ["HF_TOKEN"]
+
+
+model_path = 'meta-llama/Meta-Llama-3-8B-Instruct'
+quant_path = model_path + "_awq"
+quant_config = { "zero_point": True, "q_group_size": 128, "w_bit": 4, "version": "GEMM" }
+
+# Load model
+model = AutoAWQForCausalLM.from_pretrained(
+    model_path, **{"low_cpu_mem_usage": True, "use_cache": False, "device_map": "auto"} 
+)
+tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True, token=token)
+
+
+# Quantize
+model.quantize(tokenizer, quant_config=quant_config)
+
+# # Save quantized model
+model.save_quantized(quant_path, safetensors=True, shard_size="4GB")
+tokenizer.save_pretrained(quant_path)
+
+print(f'Model is quantized and saved at "{quant_path}"')
+
+
+
+
